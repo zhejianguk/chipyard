8a8d1b37a6ee3d20947787e1218e24d67ad406f1
zhejiang <zhejiang.uk@gmail.com>
2022-02-01 00:35:42 +0000

    Removed the unused commands.

--- generators/chipyard/src/main/scala/config/RocketConfigs.scala
+++ generators/chipyard/src/main/scala/config/RocketConfigs.scala
@@ -8,7 +8,6 @@ import freechips.rocketchip.diplomacy.{AsynchronousCrossing}
 // --------------
 
 class RocketConfig extends Config(
-  new chipyard.example.WithGCD(useAXI4=false, useBlackBox=false) ++  // Use GCD Chisel, connect Tilelink
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++         // single rocket-core
   new chipyard.config.AbstractConfig)
 
