52714c011402494f810117e87513f69e35f88591
zhejiang <zhejiang.uk@gmail.com>
2022-06-10 02:14:22 +0100

    FUlly reconfigurable.

--- sims/verilator/ght.h
+++ sims/verilator/ght.h
@@ -17,25 +17,25 @@ static inline uint64_t ght_get_status ()
   return get_status;
 }
 
-static inline void ght_cfg_rtable (uint64_t index, uint64_t func, uint64_t opcode, uint64_t sel_d)
+static inline void ght_cfg_filter (uint64_t index, uint64_t func, uint64_t opcode, uint64_t sel_d)
 {
   uint64_t set_ref;
   set_ref = ((index & 0x1f)<<4) | ((sel_d & 0xf)<<17) | ((opcode & 0x7f)<<21) | ((func & 0xf)<<28) | 0x02;
   ROCC_INSTRUCTION_SS (1, set_ref, 0X02, 0x06);
 }
 
-static inline void ght_cfg_stable_checkers (uint64_t se_id, uint64_t end_id, uint64_t policy, uint64_t start_id)
+static inline void ght_cfg_se (uint64_t se_id, uint64_t end_id, uint64_t policy, uint64_t start_id)
 {
-  uint64_t set_stable_checkers;
-  set_stable_checkers = ((se_id & 0x1f)<<4) | ((start_id & 0xf)<<17) | ((policy & 0x7f)<<21) | ((end_id & 0xf)<<28) | 0x03;
-  ROCC_INSTRUCTION_SS (1, set_stable_checkers, 0X02, 0x06);
+  uint64_t set_se;
+  set_se = ((se_id & 0x1f)<<4) | ((start_id & 0xf)<<17) | ((policy & 0x7f)<<21) | ((end_id & 0xf)<<28) | 0x04;
+  ROCC_INSTRUCTION_SS (1, set_se, 0X02, 0x06);
 }
 
-static inline void ght_cfg_stable_sch (uint64_t se_id, uint64_t m_inst_type, uint64_t index_m)
+static inline void ght_cfg_mapper (uint64_t inst_type, uint64_t ses_receiving_inst)
 {
-  uint64_t set_stable_sch;
-  set_stable_sch = ((se_id & 0x1f)<<4) | ((index_m & 0xf)<<17) | ((m_inst_type & 0x7f)<<21) | 0x04;
-  ROCC_INSTRUCTION_SS (1, set_stable_sch, 0X02, 0x06);
+  uint64_t set_mapper;
+  set_mapper = ((inst_type & 0x1f)<<4) | ((ses_receiving_inst & 0xFFFF)<<16) | 0x03;
+  ROCC_INSTRUCTION_SS (1, set_mapper, 0X02, 0x06);
 }
 
 
